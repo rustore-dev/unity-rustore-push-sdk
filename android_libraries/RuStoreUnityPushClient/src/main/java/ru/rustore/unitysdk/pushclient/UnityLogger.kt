a4d18013d89cba971aa6c57b1b433a05bb084423
rpelmegov <rpelmegov@ya.ru>
2025-03-18 01:09:16 +0300

    Added Log events to Android project

--- android_libraries/RuStoreUnityPushClient/src/main/java/ru/rustore/unitysdk/pushclient/UnityLogger.kt
+++ android_libraries/RuStoreUnityPushClient/src/main/java/ru/rustore/unitysdk/pushclient/UnityLogger.kt
@@ -9,31 +9,34 @@ class UnityLogger (
 
     override fun verbose(message: String, throwable: Throwable?) {
         Log.v(tag, message, throwable)
-        RuStoreUnityPushClient.Log("[V] $tag $message");
+        RuStoreUnityPushClient.Log("[V] $tag $message")
+        RuStoreUnityPushClient.LogVerbose("[V] $tag $message")
         logException(throwable)
     }
 
     override fun debug(message: String, throwable: Throwable?) {
         Log.d(tag, message, throwable)
-        RuStoreUnityPushClient.Log("[D] $tag $message");
+        RuStoreUnityPushClient.Log("[D] $tag $message")
+        RuStoreUnityPushClient.LogDebug("[D] $tag $message")
         logException(throwable)
     }
 
     override fun info(message: String, throwable: Throwable?) {
         Log.i(tag, message, throwable)
-        RuStoreUnityPushClient.Log("[I] $tag $message");
+        RuStoreUnityPushClient.Log("[I] $tag $message")
+        RuStoreUnityPushClient.LogInfo("[I] $tag $message")
         logException(throwable)
     }
 
     override fun warn(message: String, throwable: Throwable?) {
         Log.w(tag, message, throwable)
-        RuStoreUnityPushClient.LogWarning("[W] $tag $message");
+        RuStoreUnityPushClient.LogWarning("[W] $tag $message")
         logException(throwable)
     }
 
     override fun error(message: String, throwable: Throwable?) {
         Log.e(tag, message, throwable)
-        RuStoreUnityPushClient.LogError("[E] $tag $message");
+        RuStoreUnityPushClient.LogError("[E] $tag $message")
         logException(throwable)
     }
 
--- android_libraries/RuStoreUnityPushClient/src/main/java/ru/rustore/unitysdk/pushclient/callbacks/UnityLogListener.java
+++ android_libraries/RuStoreUnityPushClient/src/main/java/ru/rustore/unitysdk/pushclient/callbacks/UnityLogListener.java
@@ -2,8 +2,12 @@ package ru.rustore.unitysdk.pushclient.callbacks;
 
 public interface UnityLogListener {
 
+    @Deprecated
     public void Log(String logString);
+    public void LogInfo(String logString);
     public void LogWarning(String logString);
     public void LogError(String logString);
     public void LogException(Throwable throwable);
+    public void LogDebug(String logString);
+    public void LogVerbose(String logString);
 }
